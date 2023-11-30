b9565ab016a1106fb888774ac5a4ecbcf3c6edea
emresari24 <119507391+y-e-sryldz@users.noreply.github.com>
2023-11-30 16:33:03 +0300

    fitness

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for
 import pyodbc
 
 app = Flask(__name__)
@@ -12,19 +12,33 @@ driver = '{ODBC Driver 18 for SQL Server}'  # Sürücü adınızı doğru sürü
 
 conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
 
+app.secret_key = 'bu_gizli_anahtar_cok_guvenli_olmali'
+
 def connect_db():
     return pyodbc.connect(conn_str)
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        submit_button = request.form.get('submit_button')
 
-        if check_user(username, password):
-            return render_template('html/danisan.html', username=username)
-        else:
-            return render_template('html/main.html', error='Kullanıcı adı veya şifre hatalı.')
+        if submit_button == 'login':
+            username = request.form['username']
+            password = request.form['password']
+
+            if check_user(username, password):
+                return render_template('html/danisan.html', username=username)
+            else:
+                flash('Kullanıcı adı veya şifre hatalı.', 'error')
+
+        elif submit_button == 'forgot_password':
+            email = request.form['username']
+
+            if not email:
+                flash('E-posta alanı boş bırakılamaz.', 'error')
+            else:
+                # E-posta kontrolü başarılı, e-posta değerini kullan
+                return render_template('html/reset_password.html', email=email)
 
     return render_template('html/main.html')
 
@@ -35,6 +49,39 @@ def check_user(username, password):
         result = cursor.execute(query, (username, password)).fetchone()
 
         return result is not None
+
+
+def update_user_password(email, new_password):
+    # Azure Database bağlantısını yap
+    conn_str = f'DRIVER={driver};SERVER={server};DATABASE={database};UID={username};PWD={password}'
+
+    try:
+        with pyodbc.connect(conn_str) as conn:
+            cursor = conn.cursor()
+            # Kullanıcının şifresini güncelle
+            query = "UPDATE kullanicilar SET sifre = ? WHERE e_posta = ?"
+            cursor.execute(query, (new_password, email))
+            conn.commit()
+
+            # Başarı mesajı ekleyebilirsiniz
+            flash('Şifreniz başarıyla değiştirildi.', 'success')
+    except Exception as e:
+        # Hata durumunda kullanıcıya bilgi verebilirsiniz
+        flash(f'Hata oluştu: {str(e)}', 'danger')
+
+@app.route('/reset_password', methods=['POST'])
+def reset_password():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        new_password = request.form.get('new_password')
+
+        # Veritabanındaki şifreyi güncelle
+        update_user_password(email, new_password)
+
+        # Kullanıcıyı ana sayfaya yönlendir
+        return redirect(url_for('home'))
+
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
