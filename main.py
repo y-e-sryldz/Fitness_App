e7a7e695084c2781ed2f120518dc51c8451f62af
emresari24 <119507391+y-e-sryldz@users.noreply.github.com>
2023-12-01 23:19:28 +0300

    fitness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, session
 import pyodbc
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
@@ -26,8 +27,13 @@ def home():
             username = request.form['username']
             password = request.form['password']
 
+            user_id = get_user_id_by_email(username)
+            print(user_id)
+            session['user_id'] = user_id
+
             if check_user(username, password):
-                return render_template('html/danisan.html', username=username)
+
+                return render_template('html/danisan.html', username=username, user_id=user_id)
             else:
                 flash('Kullanıcı adı veya şifre hatalı.', 'error')
 
@@ -42,13 +48,27 @@ def home():
 
     return render_template('html/main.html')
 
+#giriş kontrolü sağlayan kod parçası
 def check_user(username, password):
     with connect_db() as conn:
         cursor = conn.cursor()
         query = "SELECT * FROM kullanicilar WHERE e_posta = ? AND sifre = ?"
         result = cursor.execute(query, (username, password)).fetchone()
-
         return result is not None
+
+
+#kullanıcı adını tuutan id fonksiyonu
+def get_user_id_by_email(email):
+    with connect_db() as conn:
+        cursor = conn.cursor()
+        query = "SELECT id FROM kullanicilar WHERE e_posta = ?"
+        result = cursor.execute(query, (email,)).fetchone()
+
+        if result:
+            return result[0]  # id değerini döndür
+        else:
+            return None  # Eğer e-posta bulunamazsa None döndür
+
 
 def update_user_password(email, new_password):
     # Azure Database bağlantısını yap
@@ -81,6 +101,51 @@ def reset_password():
         return redirect(url_for('home'))
 
     return redirect(url_for('home'))
+
+@app.route('/kisisel_bilgi_ekle', methods=['POST'])
+def kisisel_bilgi_ekle():
+    if request.method == 'POST':
+
+        user_id = session.get('user_id')
+        print(user_id)
+
+        # Formdan gelen verileri al
+        ad = request.form['ad']
+        sifre = request.form['sifre']
+        dogum_tarihi = request.form['dogum-tarihi']
+        cinsiyet = request.form['cinsiyet']
+        email = request.form['email']
+        telefon = request.form['telefon']
+
+        # Formdan gelen dosyayı al
+        profil_foto = request.files['profil-foto']
+
+        # Dosya adını güvenli bir şekilde al
+        guvenli_dosya_adi = secure_filename(profil_foto.filename)
+
+        # Dosyayı kaydetmek için uygun bir klasöre kaydet
+        dosya_yolu = f'uploads/{guvenli_dosya_adi}'
+        profil_foto.save(dosya_yolu)
+
+        # Veritabanına ekleme işlemleri
+        try:
+            with connect_db() as conn:
+                cursor = conn.cursor()
+
+                # Kullanıcı tablosuna ekle
+                cursor.execute("UPDATE kullanicilar SET adi=?, e_posta=?, sifre=?, rol=? WHERE id=?", (ad, email, sifre, 3, user_id))
+                conn.commit()
+                # Danışanlar tablosuna ekle
+                cursor.execute("UPDATE Danisanlar SET dogum_tarihi=?, cinsiyet=?, telefon_numarasi=?, pp=? WHERE id=?", (dogum_tarihi, cinsiyet, telefon, dosya_yolu, user_id))
+                conn.commit()
+
+                print('Bilgiler başarıyla kaydedildi.', 'success')
+
+        except Exception as e:
+            # Hata durumunda kullanıcıya bilgi ver
+            print(f'Hata oluştu: {str(e)}', 'danger')
+    return render_template('html/danisan.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
